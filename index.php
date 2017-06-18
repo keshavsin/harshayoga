5baa4707dc0ba49c79cedf1714c9594d19aa5176
Keshav Habib <keshavsin@gmail.com>
2017-06-18 13:17:28 +0530

    Fixed issues with distortion in home page

--- index.php
+++ index.php
@@ -2,37 +2,37 @@
 $currentPage = "index";
 include 'common/header.php';
 ?>
-  <section>
-  <div id="myCarousel" class="carousel slide main_slider" data-ride="carousel">
-  <div class="carousel-inner " role="listbox">
-    <div class="item active">
-      <img src="assets/images/main-slider/slide1.jpg" alt="">
+   <section>
+      <div id="myCarousel" class="carousel slide main_slider" data-ride="carousel">
+        <div  class="carousel-inner " role="listbox">
+            <div class="item active">
+              <img src="assets/images/main-slider/slide1.jpg" alt="">
+            </div>
+            <div class="item">
+              <img src="assets/images/main-slider/slide2.jpg" alt="">
+            </div>
+            <div class="item">
+              <img src="assets/images/main-slider/slide3.jpg" alt="">
+            </div>
+            <div class="item">
+              <img src="assets/images/main-slider/slide4.jpg" alt="">
+            </div>
+        	<div class="item">
+              <img src="assets/images/main-slider/slide5.jpg" alt="">
+            </div>
+        	<div class="item">
+              <img src="assets/images/main-slider/slide6.jpg" alt="">
+            </div>
+        </div>
+        <div class="slide_caption">
+        </div>
+          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+              <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+             <span class="sr-only">Next</span>
+          </a>
     </div>
-    <div class="item">
-      <img src="assets/images/main-slider/slide2.jpg" alt="">
-    </div>
-    <div class="item">
-      <img src="assets/images/main-slider/slide3.jpg" alt="">
-    </div>
-    <div class="item">
-      <img src="assets/images/main-slider/slide4.jpg" alt="">
-    </div>
-	<div class="item">
-      <img src="assets/images/main-slider/slide5.jpg" alt="">
-    </div>
-	<div class="item">
-      <img src="assets/images/main-slider/slide6.jpg" alt="">
-    </div>
-  </div>
-  <div class="slide_caption">
-  </div>
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-     <span class="sr-only">Next</span>
-  </a>
-</div>
   </section>
   <section>
     <div class="container-fluid">
@@ -60,54 +60,59 @@ Along with the above, the students will get to experience the best teachings.
 </p>
         </div>
   </section>
-   <section class="container-fluid">
+
+
+
+<section class="container-fluid">
       <div class="row table_container">
       
         <div class="col-sm-6 table_cell cell1">
         	<h2 class="heading1 colr_h">WHY US?</h2>
             <p>Harsha has practiced the Iyengar’s teachings techniques &amp; the traditional Ashtanga Vinyasa along with years of hatha yoga practice. With the strong knowledge base of the major scriptures backed with the therapeutic knowledge is indeed a blessing for an aspiring teacher to learn. With a decade worth of studies &amp; practice, the experience is unmatched.
-<br/><br/>
-Asana practice, Yoga philosophy, Anatomy, Hatha yoga, Yoga sutras, you name it &amp; you'll get answer to all ur questions. With a strong foundation of all the above, participants will able to chart their own path in this journey of yoga.
-</p>
+            <br/><br/>
+            Asana practice, Yoga philosophy, Anatomy, Hatha yoga, Yoga sutras, you name it &amp; you'll get answer to all ur questions. With a strong foundation of all the above, participants will able to chart their own path in this journey of yoga.
+            </p>
+            <br><br><br><br><br>
         </div>
         <div class="col-sm-6 table_cell bg_container1">
         	
         </div>
       </div>
-  </section>
-  <section>
-    <div class="container-fluid">
-      <div class="row table_container">
-      <div class="col-sm-6 table_cell bg_container2">
+</section>
 
-        </div>
-        <div class="col-sm-6 table_cell cell1">
-        	<h2 class="heading1 colr_h">Ashtanga Yoga</h2>
+
+
+<section class="container-fluid">
+  <div class="row table_container">
+    <div class="col-sm-6 table_cell  bg_container2"> 
+    </div>
+    <div class="col-sm-6 table_cell cell1">
+          <h2 class="heading1 colr_h">Ashtanga Yoga</h2>
             <p><ul>
-			<li>Primary series</li>
-			<li>Intermediate series</li> 
-			<li>Advanced series</li>
-			</ul></p>
-        </div>
+                <li>Primary series</li>
+                <li>Intermediate series</li> 
+                <li>Advanced series</li>
+              </ul></p>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> 
+    </div>
+</section>
+
+<section class="container-fluid">
+    <div class="col-sm-12 row table_container">
+      <div class="col-sm-6 table_cell cell1">
+        	<h2 class="heading1 colr_h">Hatha Yoga</h2>
+        			<ul>
+          			<li>Shatkarma </li>
+          			<li>Asana & pranayama</li> 
+          			<li>Mudra & bandha</li>
+        			</ul>
+              <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      </div>
+      <div class="col-sm-6 table_cell bg_container3">
       </div>
     </div>
   </section>
-  <section class="container-fluid">
-      <div class="row table_container">
-      
-        <div class="col-sm-6 table_cell cell1">
-        	<h2 class="heading1 colr_h">Hatha Yoga</h2>
-			<ul>
-			<li>Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma Shatkarma </li>
-			<li>Asana & pranayama</li> 
-			<li>Mudra & bandha</li>
-			</ul>
-        </div>
-        <div class="col-sm-6 table_cell bg_container3">
 
-        </div>
-      </div>
-  </section>
   <section>
     <div class="container-fluid">
       <div class="row table_container">
@@ -116,15 +121,17 @@ Asana practice, Yoga philosophy, Anatomy, Hatha yoga, Yoga sutras, you name it &
         </div>
         <div class="col-sm-6 table_cell cell1">
         	<h2 class="heading1 colr_h">Yoga Therapy</h2>
-			<p><ul>
-			<li>Yoga anatomy</li>
-			<li>Yoga for motherhood</li> 
-			<li>Yoga for women's health</li>
-			</ul></p>
+      			<p><ul>
+          			<li>Yoga anatomy</li>
+          			<li>Yoga for motherhood</li> 
+          			<li>Yoga for women's health</li>
+      			</ul></p>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
         </div>
       </div>
     </div>
   </section>
+
   <section class="gradiant_bg">
   	<div class="innr_container">
     	<h2 class="heading1 cntr_h">DHARMA</h2>
