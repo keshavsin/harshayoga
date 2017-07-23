25d735a1cbe00a300c87eda571b5908b29dd0c15
Amit Sinha <ak5792@gmail.com>
2017-07-23 22:13:06 +0530

    Update and rename yashaswini.php to chethan.php

---
+++ chethan.php
@@ -0,0 +1,37 @@
+<?php 
+session_start();
+$currentPage = "about";
+include 'common/header.php';
+?>
+  <section id="about_harsha">
+      <div class="about_innr">
+      <div class="container-fluid">
+        <div class="row">
+                <div class="col-xs-12 about_text">
+                 <h2 class="heading1 colr_h">Chethan Nagaraj</h2>
+				 <br>
+			<p>
+			<img src="assets/images/yashaswini.jpg" class="floating_thumbr" alt="iyengar" />Namaste,<br/><br/>
+				
+				Chethan Nagaraj is a senior Iyengar yoga teacher with in-depth knowledge in the Guruji iyengars teaching techniques. Chethan has been practicing for almost a decade now and has practiced with senior Iyengar yoga teacher Mr.Muralidhar for years.
+
+				<br/><br/>
+				Mastery in the usage of porps to further deepen the practice has helped students in correcting the alignment and helping them gain a deeper understanding of body & mind.
+
+				<br/><br/>
+				Students love his classes as it helps them learn in achieving a deeper level of relaxation and heal , balancing all the fluctuations of mind.
+				<br/><br/>
+				Chethan is Harsha’s elder brother. The yogi brothers started their journey in a very early age traveling 10 kms everyday early in the morning for years. The interest in deepening the practice has led them to where they are. Coming from nothing , both of them have been molded into one of the finest teachers.
+				<br><br><br>
+				<b><i>Chethan currently teaches at the Yoga Patahshala in bangalore. The brothers have started teaching yoga all round the globe with worshops & retreats.And will soon be starting advance training programs too. </i></b>
+				<br/><br/>
+				<br/>
+			</p>
+		</div>
+   	</div>
+    </div>
+    </div>
+    </section>
+ <?php include 'common/footer.php';?>
+</body>
+</html>
