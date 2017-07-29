fb705fe5274dc98a47d93782ba6c2c12e2ed9f86
Amit Sinha <ak5792@gmail.com>
2017-07-30 00:17:11 +0530

    Update footer.php

--- common/footer.php
+++ common/footer.php
@@ -1,11 +1,19 @@
 <footer class="footer_main text-center">
   	<h2 class="footer_h">Harsha Yoga Pathashala</h2>
-    <div class="footer_address">
-		<h3 class="heading2 text-center">(Bangalore)</h3>
-      Ideal Homes Main Road, Above Sangeetha Mobiles<br/>
-Rajarajeshwarinagar, Bangalore.<br/>
-Email: <a href="mailto:harsha@harshayoga.com">harsha@harshayoga.com</a><br/>
-      </div>
+		<div class="row text-center">
+			<div class="footer_address col-xs-12 col-sm-6 col-md-5 col-md-offset-1 col-lg-4 col-lg-offset-2">
+				<h3 class="heading2 text-center">(Bangalore)</h3>
+				Ideal Homes Main Road, Above Sangeetha Mobiles<br/>
+				Rajarajeshwarinagar, Bangalore.<br/>
+				Email: <a href="mailto:harsha@harshayoga.com">harsha@harshayoga.com</a><br/>
+			</div>
+			<div class="footer_address col-xs-12 col-sm-6 col-md-5 col-lg-4 hide">
+				<h3 class="heading2 text-center">(GOA)</h3>
+				Ideal Homes Main Road, Above Sangeetha Mobiles<br/>
+				Rajarajeshwarinagar, Bangalore.<br/>
+				Email: <a href="mailto:harsha@harshayoga.com">harsha@harshayoga.com</a><br/>
+			</div>
+		</div>
       <div class="footer_small">
           <div class="row">
             <div class="col-md-7">
