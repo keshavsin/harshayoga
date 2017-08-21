4cff12a2050d5d4ddbdaef1b3b47e98581f55128
Keshav Habib <keshavsin@gmail.com>
2017-08-21 17:51:04 +0530

    Removed the white background from favicon

--- base/common/dbconf.php
+++ base/common/dbconf.php
@@ -1,15 +1,17 @@
 <?php
-$server = '43.255.154.108';
+// $database =  'hyogaprod';
 //$username = 'hy_db_produser';
 //$password = 'hyprod11!!';
-// $server = 'localhost';
-// $username = 'root';
-// $password = '';
-// $database =  'hyogaprod';
-//$server = 'localhost';
-$username = 'hyogaprod07';
-$password = 'hyogaprod@07';
-$database =  'hyogaprod07';
+  
+ $server = 'localhost';
+ $database =  'hyogaprod07';
+ $username = 'root';
+ $password = '';
+
+//$server = '43.255.154.108';
+//$username = 'hyogaprod07';
+//$password = 'hyogaprod@07';
+//$database =  'hyogaprod07';
 $db = mysqli_connect($server,$username,$password,$database);
 ?>
 
